f8798d9a859a0e576671dca0b998d3c1a2cd8ba2
isaachrr4 <80857536+isaachrr4@users.noreply.github.com>
2021-07-14 13:25:51 -0400

    added routes

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ const app =express()
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-app.use(express.static("public"))
+app.use(express.static("./devlop/public"))
 
+app.get('/notes', (req,res)=>{
+    res.sendFile(path.join(__dirname,"./develop/public/notes.html"))
+    })
+
+    app.get('*', (req,res)=>{
+        res.sendFile(path.join(__dirname,"./develop/public/index.html"))
+        })
+
+        app.get('/notes', (req, res) => {
+            res.json((notes))
+          });
+
+          app.post('/notes', (req, res) => {
+            notes.push(req.body)
+            res.json(notes)
+          });
 app.listen(PORT,() =>{console.log("Server is Listening");})
